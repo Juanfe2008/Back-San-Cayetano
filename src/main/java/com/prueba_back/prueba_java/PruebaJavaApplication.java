ffba528683fff7233f437806a8bfbff1d43b1778
Juanfe2008 <wardkjuan44@gmail.com>
2022-11-24 17:38:37 -0500

    config de feign

--- src/main/java/com/prueba_back/prueba_java/PruebaJavaApplication.java
+++ src/main/java/com/prueba_back/prueba_java/PruebaJavaApplication.java
@@ -2,8 +2,10 @@ package com.prueba_back.prueba_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PruebaJavaApplication {
 
     public static void main(String[] args) {
